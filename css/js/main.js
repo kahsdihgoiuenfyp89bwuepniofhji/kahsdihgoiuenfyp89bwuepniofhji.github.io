b364285cd227c129d2184efb5f9eb98d95c1c1f5
kahsdihgoiuenfyp89bwuepniofhji <clientmanagement1@webelodesigns.com>
2025-06-25 09:17:18 -0600

    Delete css/js directory

--- css/js/main.js
+++
@@ -1,7 +0,0 @@
-
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.querySelector('.navbar ul');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
